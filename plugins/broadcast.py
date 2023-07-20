6a84a52726bdac4a1f96ba8f0b1ad597073b8b84
Aman chaudhary <123990844+theamanchaudhary@users.noreply.github.com>
2023-07-21 01:13:45 +0530

    Update broadcast.py

--- plugins/broadcast.py
+++ plugins/broadcast.py
@@ -37,9 +37,9 @@ async def verupikkals(bot, message):
         done += 1
         await asyncio.sleep(2)
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")    
+            await sts.edit(f"Broadcast in progress:\n\n❤️Total Users {total_users}\n✅Completed: {done} / {total_users}\nSuccess: {success}\n🚫Blocked: {blocked}\nDeleted: {deleted}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Users {total_users}\nCompleted: {done} / {total_users}\nSuccess: {success}\nBlocked: {blocked}\nDeleted: {deleted}")
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\n❤️Total Users {total_users}\n✅Completed: {done} / {total_users}\n😍Success: {success}\n🚫Blocked: {blocked}\n🙂Deleted: {deleted}")
 
 @Client.on_message(filters.command("grp_broadcast") & filters.user(ADMINS) & filters.reply)
 async def broadcast_group(bot, message):
@@ -62,7 +62,7 @@ async def broadcast_group(bot, message):
                 failed += 1
         done += 1
         if not done % 20:
-            await sts.edit(f"Broadcast in progress:\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nSuccess: {success}")    
+            await sts.edit(f"Broadcast in progress:\n\n❤️Total Groups {total_groups}\n✅Completed: {done} / {total_groups}\n😍Success: {success}")    
     time_taken = datetime.timedelta(seconds=int(time.time()-start_time))
-    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\nTotal Groups {total_groups}\nCompleted: {done} / {total_groups}\nSuccess: {success}")
+    await sts.edit(f"Broadcast Completed:\nCompleted in {time_taken} seconds.\n\n❤️Total Groups {total_groups}\n✅Completed: {done} / {total_groups}\n😍Success: {success}")
         
