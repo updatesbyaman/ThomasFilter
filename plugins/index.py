fa3089a2eb509fc2ffe19586ec6eb7cfaee7c281
Aman chaudhary <123990844+theamanchaudhary@users.noreply.github.com>
2023-07-21 01:08:39 +0530

    Update index.py

--- plugins/index.py
+++ plugins/index.py
@@ -18,7 +18,7 @@ lock = asyncio.Lock()
 async def index_files(bot, query):
     if query.data.startswith('index_cancel'):
         temp.CANCEL = True
-        return await query.answer("Cancelling Indexing")
+        return await query.answer("Cancelling Indexing 🥲")
     _, raju, chat, lst_msg_id, from_user = query.data.split("#")
     if raju == 'reject':
         await query.message.delete()
@@ -34,12 +34,12 @@ async def index_files(bot, query):
     await query.answer('Processing...⏳', show_alert=True)
     if int(from_user) not in ADMINS:
         await bot.send_message(int(from_user),
-                               f'Your Submission for indexing {chat} has been accepted by our moderators and will be added soon.',
+                               f'Your Submission for indexing {chat} has been accepted by our moderators and will be added soon.❤️',
                                reply_to_message_id=int(lst_msg_id))
     await msg.edit(
         "Starting Indexing",
         reply_markup=InlineKeyboardMarkup(
-            [[InlineKeyboardButton('Cancel', callback_data='index_cancel')]]
+            [[InlineKeyboardButton('Cancel', callback_data='🙂 index_cancel 🙂')]]
         )
     )
     try:
