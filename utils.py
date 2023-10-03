bfefcaa34c15f9ded906ea233a74637d8838c88f
Aman chaudhary <123990844+theamanchaudhary@users.noreply.github.com>
2023-10-03 17:18:05 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -65,6 +65,67 @@ async def is_subscribed(bot, query):
             return True
 
     return False
+async def direct_gen_handler(m: Message):
+     if not DIRECT_GEN:
+         return None, None
+     try:
+         log_msg = await m.copy(chat_id=DIRECT_GEN_DB)
+         stream_link, download_link = await gen_link(log_msg)
+         if stream_link and download_link:
+             if not m.reply_markup:
+                 markup = InlineKeyboardMarkup(
+                 [
+                     [
+                         InlineKeyboardButton("🎥 Stream 🎥", url=stream_link),
+                         InlineKeyboardButton("📥 Download 📥", url=download_link),
+                     ]
+                 ]
+             )
+             else:
+                 markup = m.reply_markup.inline_keyboard.copy()
+                 markup.insert(
+                     0,
+                     [
+                         InlineKeyboardButton("🎥 Stream 🎥", url=stream_link),
+                         InlineKeyboardButton("📥 Download 📥", url=download_link),
+                     ]
+                 )
+                 markup = InlineKeyboardMarkup(markup)
+             return markup
+     except FloodWait as e:
+         await asyncio.sleep(e.value)
+         await direct_gen_handler(m)
+
+
+ # Direct Link Generator
+ async def gen_link(log_msg):
+     page_link = f"{DIRECT_GEN_URL}watch/{get_hash(log_msg)}{log_msg.id}"
+     stream_link = f"{DIRECT_GEN_URL}{log_msg.id}/{quote_plus(get_name(log_msg))}?hash={get_hash(log_msg)}"
+     return page_link, stream_link
+
+ def get_media_from_message(message: "Message"):
+     media_types = (
+         "audio",
+         "document",
+         "photo",
+         "sticker",
+         "animation",
+         "video",
+         "voice",
+         "video_note",
+     )
+     for attr in media_types:
+         if media := getattr(message, attr, None):
+             return media
+
+ def get_name(media_msg: Message) -> str:
+     media = get_media_from_message(media_msg)
+     return getattr(media, "file_name", "None")
+
+
+ def get_hash(media_msg: Message) -> str:
+     media = get_media_from_message(media_msg)
+     return getattr(media, "file_unique_id", "")[:6]
 
 async def get_poster(query, bulk=False, id=False, file=None):
     if not id:
